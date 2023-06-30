3e5912aae467a08ad28f3231c32d6d90f3791bec
Juan Vena <juan2645@gmail.com>
2023-06-30 20:38:42 -0300

    Se agrego la funcion menuUsuarios dentro de socios.js

--- js/socios.js
+++ js/socios.js
@@ -1,34 +1,44 @@
 function socios() {
-
     const centralBody = document.getElementById("central-body");
     const textoBotonSocios = `
-    <head>
-        <script src="js/menuUsuarios.js"></script>
-    </head>
-    
-    <div class="titulo">Socios</div>
+        <div class="titulo">Socios</div>
         <div class="login-form">
-                <form>
-                    <div class="form-group">
+            <form>
+                <div class="form-group">
                     <label for="username">Usuario</label>
                     <input type="text" id="username" name="username">
-                    </div>
-                    <div class="form-group">
+                </div>
+                <div class="form-group">
                     <label for="password">Contraseña</label>
                     <input type="password" id="password" name="password">
-                    </div>
-                    <button class="login-button" id="register-button">Loguearse</button>
-                </form>
-                <p class="texto_registro">No estás registrado? <a href="registro.html">¡Regístrate aquí!</a></p>
+                </div>
+                <button class="login-button" id="register-button">Loguearse</button>
+            </form>
+            <p class="texto_registro">No estás registrado? <a href="registro.html">¡Regístrate aquí!</a></p>
         </div>
-    </div>
-
-    <script>
-        const botonLoguearse = document.getElementById('register-button');
-        botonLoguearse.addEventListener('click', menuUsuarios);
-    </script>
     `;
 
     centralBody.innerHTML = "";
     centralBody.innerHTML = textoBotonSocios;
+
+    const botonLoguearse = document.getElementById('register-button');
+    botonLoguearse.addEventListener('click', function (e) {
+        e.preventDefault();
+        menuUsuarios(); 
+    });
+}
+
+
+function menuUsuarios() {
+    const centralBody = document.getElementById("central-body");
+    const textoMenuUsuarios = `
+        <div class="sidebar2">
+            <div><button id="btnCuotas" class="normal">Cuotas</button></div>
+            <div><button id="btnBeneficios" class="normal">Beneficios</button></div>
+            <div><button id="btnSugerencias" class="normal">Sugerencias</button></div>
+        </div>
+    `;
+
+    centralBody.innerHTML = "";
+    centralBody.innerHTML = textoMenuUsuarios;
 }
