0b1abb0f2802123297b8fc7f7fec677d8a2e3f70
Juan Vena <juan2645@gmail.com>
2023-07-01 00:07:14 -0300

    Se agregan enlaces al menuUsuarios

--- js/socios.js
+++ js/socios.js
@@ -28,7 +28,6 @@ function socios() {
     });
 }
 
-
 function menuUsuarios() {
     const centralBody = document.getElementById("central-body");
     const textoMenuUsuarios = `
@@ -38,7 +37,105 @@ function menuUsuarios() {
             <div><button id="btnSugerencias" class="normal">Sugerencias</button></div>
         </div>
     `;
-
+    
     centralBody.innerHTML = "";
     centralBody.innerHTML = textoMenuUsuarios;
+
+    const btnCuotas = document.getElementById('btnCuotas');
+    btnCuotas.addEventListener('click', function (e) {
+       e.preventDefault();
+       console.log("Función cuotas() antes de llamarla");
+        cuotas();
+    });
+
+    const btnBeneficios = document.getElementById('btnBeneficios');
+    btnBeneficios.addEventListener('click', function (e) {
+        e.preventDefault();
+        mostrarBeneficios();
+    });
+
+    const btnSugerencias = document.getElementById('btnSugerencias');
+    btnSugerencias.addEventListener('click', function (e) {
+        e.preventDefault();
+        mostrarSugerencias();
+    });
+}
+
+function cuotas() {
+    console.log("Función cuotas() llamada correctamente");
+    const centralBody = document.getElementById("central-body");
+    const textoMenuUsuarios1 = `
+    <div class="titulo">Socios - Cuotas</div>
+            <div id="info-container">
+                <h2 class="titulo-informacion">Información de las cuotas del socio:</h2>
+                <table class="tabla-informacion">
+                    <thead>
+                        <tr>
+                            <th class="encabezado-tabla">Nro. Cuota</th>
+                            <th class="encabezado-tabla">Mes</th>
+                            <th class="encabezado-tabla">Monto</th>
+                            <th class="encabezado-tabla">Fecha de Vencimiento</th>
+                            <th class="encabezado-tabla">Fecha de Pago</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="dato-tabla">1</td>
+                            <td class="dato-tabla">Enero 2023</td>
+                            <td class="dato-tabla">$100</td>
+                            <td class="dato-tabla">10 de febrero de 2023</td>
+                            <td class="dato-tabla">05 de febrero de 2023</td>
+                        </tr>
+                        <tr>
+                            <td class="dato-tabla">2</td>
+                            <td class="dato-tabla">Febrero 2023</td>
+                            <td class="dato-tabla">$100</td>
+                            <td class="dato-tabla">10 de marzo de 2023</td>
+                            <td class="dato-tabla">10 de marzo de 2023</td>
+                        </tr>
+                        <tr>
+                            <td class="dato-tabla">3</td>
+                            <td class="dato-tabla">Marzo 2023</td>
+                            <td class="dato-tabla">$100</td>
+                            <td class="dato-tabla">10 de abril de 2023</td>
+                            <td class="dato-tabla">04 de abril de 2023</td>
+                        </tr>
+                        <tr>
+                            <td class="dato-tabla">4</td>
+                            <td class="dato-tabla">Abril 2023</td>
+                            <td class="dato-tabla">$100</td>
+                            <td class="dato-tabla">10 de mayo de 2023</td>
+                            <td class="dato-tabla">09 de mayo de 2023</td>
+                        </tr>
+                        <tr>
+                            <td class="dato-tabla">5</td>
+                            <td class="dato-tabla">Mayo 2023</td>
+                            <td class="dato-tabla">$100</td>
+                            <td class="dato-tabla">10 de junio de 2023</td>
+                            <td class="dato-tabla">05 de junio de 2023</td>
+                        </tr>
+                        <tr>
+                            <td class="dato-tabla">6</td>
+                            <td class="dato-tabla">Junio 2023</td>
+                            <td class="dato-tabla">$100</td>
+                            <td class="dato-tabla">10 de julio de 2023</td>
+                            <td class="dato-tabla">Aún no pagada</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p class="texto-informacion">Estado: Al día</p>
+                <p class="texto-informacion">Próximo vencimiento de la cuota: 10 de agosto de 2023</p>
+            </div>
+    `;
+
+    centralBody.innerHTML = "";
+    centralBody.innerHTML = textoMenuUsuarios1;
+}
+
+function mostrarBeneficios() {
+    
+}
+
+function mostrarSugerencias() {
+    
 }
