1bc6705abbfcb819e35ebd24814c4b64c54f9e25
claudiocarmusciano <claudiocarmusciano@gmail.com>
2023-06-30 21:14:17 -0300

    se agrego la galeria de fotos

--- js/contacto.js
+++ js/contacto.js
@@ -4,18 +4,20 @@ function contacto() {
     const textoBotonContacto = `
         <div class="titulo">Contacto</div>
         <div class="sector">
-            <h3 class="textoH3"> CONTACTANOS </h3><br>
-            <h5> Dirección: </h5>
-            <p> Av Sarmiento 3093, Olavarría, Provincia de Buenos Aires </p><br>
+            <div class="textos-sector">
+                <h2 class="textoH3"> CONTACTANOS </h2><br>
+                <h5> Dirección: </h5>
+                <p> Av Sarmiento 3093, Olavarría, Provincia de Buenos Aires </p><br>
 
-            <h5> Teléfono: </h5>
-            <p> 02284 41-1682 </p><br>
-            
-            <h5> Whatsapp: </h5>
-            <p> 02284 59-3021 </p> <br>
-             
-            <p> Mail: </p>
-            <p> sfmarianomoreno@gmail.com </p>
+                <h5> Teléfono: </h5>
+                <p> 02284 41-1682 </p><br>
+                
+                <h5> Whatsapp: </h5>
+                <p> 02284 59-3021 </p> <br>
+                
+                <p> Mail: </p>
+                <p> sfmarianomoreno@gmail.com </p>
+            </div>
         </div>
     `;
 
--- js/galeria.js
+++ js/galeria.js
@@ -6,7 +6,15 @@ function galeria() {
     <div class="sector">
         <div class="images">
             <img src="images/galeria/img01.jpg" alt="">
-  
+            <img src="images/galeria/img02.jpg" alt="">
+            <img src="images/galeria/img03.jpg" alt="">
+            <img src="images/galeria/img04.jpg" alt="">
+            <img src="images/galeria/img05.jpg" alt="">
+            <img src="images/galeria/img06.jpg" alt="">
+            <img src="images/galeria/img07.jpg" alt="">
+            <img src="images/galeria/img08.jpg" alt="">
+            <img src="images/galeria/img09.jpg" alt="">
+            <img src="images/galeria/img10.jpg" alt="">
         </div>
     </div>
     `;
